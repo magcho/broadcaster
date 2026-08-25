120486eb0752645ed4b49bc31294ee1dbf9e0f87
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2026-08-25 23:39:19 +0900

    fix: remove unused waku routers

--- packages/admin/src/ui/pages/_api/api/auth/[...route].ts
+++
@@ -1,8 +0,0 @@
-import { auth } from "../../../../../libs/better-auth/server.js"
-
-export const GET = async (request: Request): Promise<Response> => {
-  return auth.handler(request)
-}
-export const POST = async (request: Request): Promise<Response> => {
-  return auth.handler(request)
-}
--- packages/admin/src/ui/pages/index.tsx
+++
@@ -1,5 +0,0 @@
-import { unstable_redirect } from "waku/router/server"
-
-export default async function HomePage() {
-  unstable_redirect("/message/send")
-}
